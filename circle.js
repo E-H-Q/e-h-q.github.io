2cd17514938f4587f046f4987066aeb5fba673ee
Benjamin Zacharias <benzach0421@gmail.com>
2025-10-13 15:03:13 -0600

    Update circle.js

--- circle.js
+++ circle.js
@@ -1,56 +1,56 @@
+// CIRCLE.JS: BRESENHAM CIRCLE ALGORITHM FOR MOVEMENT RANGE
+
 function circle(xc, yc, r) {
-	width = height = size;
+	const width = size;
+	const height = size;
 	array = new Uint8Array(width * height);
 
 	if (r < 1) return;
 
-	var x = r, y = 0,  // for Bresenham / mid-point circle
-		cd = 0,
-		xoff = 0,
-		yoff = r,
-		b = -r,
-		p0, p1, w0, w1;
+	let xoff = 0;
+	let yoff = r;
+	let b = -r;
 
 	while (xoff <= yoff) {
-		p0 = xc - xoff;
-		p1 = xc - yoff;
-		w0 = xoff + xoff;
-		w1 = yoff + yoff;
+		const p0 = xc - xoff;
+		const p1 = xc - yoff;
+		const w0 = xoff + xoff;
+		const w1 = yoff + yoff;
 
-		hl(p0, yc - yoff, yc + yoff, w0);  // fill a "line"
+		hl(p0, yc - yoff, yc + yoff, w0);
 		hl(p1, yc - xoff, yc + xoff, w1);
 
-		if ((b += xoff+++xoff) >= 0) {
+		if ((b += xoff++ + xoff) >= 0) {
 			b -= --yoff + yoff;
 		}
 	}
 
-	// for fill
 	function hl(x, y1, y2, w) {
 		w++;
-		var xw = 0;
 		while (w--) {
-		xw = x + w;
-		setPixel(xw, y1);
-		setPixel(xw, y2);
+			const xw = x + w;
+			setPixel(xw, y1);
+			setPixel(xw, y2);
 		}
 	}
 
 	function setPixel(x, y) {
-		if (x < width && y < height && x >= 0 && y >= 0)
+		if (x >= 0 && x < width && y >= 0 && y < height) {
 			array[y * width + x] = 1;
+		}
 	}
 }
 
 function convert() {
 	pts = [];
-	var conv = Array.from(array); // converts uint8Array into a normal array
+	const conv = Array.from(array);
 	while(conv.length) {
 		pts.push(conv.splice(0, size));
 	}
-	for (i = 0; i < walls.length; i++) {
+	
+	for (let i = 0; i < walls.length; i++) {
 		pts[walls[i].x][walls[i].y] = 0;
 	}
 
-	array = new Uint8Array(width * height);
+	array = new Uint8Array(size * size);
 }
