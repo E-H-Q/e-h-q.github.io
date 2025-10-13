e3f4c87f9c3ba9eefbeca279e057ed1d17b3f43b
Benjamin Zacharias <benzach0421@gmail.com>
2025-10-13 15:04:57 -0600

    Update io.js

--- io.js
+++ io.js
@@ -1,8 +1,10 @@
 // IO.JS: HANDLES LOADING + SAVING MAP DATA
 
 function KILL_ENEMY() { // here temporarily, just a sloppy function passing through ;^)
-	enemy.hp = 0;
-	update();
+	for (var i = 0; i < allEnemies.length; i++) {
+		allEnemies[i].hp = 0;
+		update();
+	}
 }
 
 function save_map() {
@@ -16,10 +18,10 @@ function save_map() {
 }
 
 function load_map() {
-	input = document.getElementById("file");
+	file_input = document.getElementById("file");
 
-	if (input.files.length == 0) return false;
-	const load = input.files[0];
+	if (file_input.files.length == 0) return false;
+	const load = file_input.files[0];
 	let reader = new FileReader();
 
 	reader.onload = (event) => {
@@ -35,39 +37,3 @@ function load_map() {
 	};
 	reader.readAsText(load);
 }
-
-/*
-function save_map() {
-	text = walls;
-	name = "map";
-	type = "text/plain";
-
-	var file = new Blob([size + "\n" + text], {type: type});
-	save_button.href = URL.createObjectURL(file);
-	save_button.download = name;
-}
-
-function load_map() {
-	var input = document.getElementById("file");
-
-	if (input.files.length == 0) return false;
-	const load = input.files[0];
-	let reader = new FileReader();
-
-	reader.onload = (event) => {
-		const load = event.target.result;
-
-		var lines = load.split('\n');
-		size = lines[0];
-		var loaded = lines[1].split(","); // reads the second line (map data)
-
-		walls = [];
-		while (loaded.length) {
-			walls.push(loaded.splice(0, size));
-			console.log(loaded);
-		}
-		update();
-	};
-	reader.readAsText(load);
-}
-*/
