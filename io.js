43b98e15dd499bf679a688b157d2c97e46d0547b
benzach0421 <benzach0421@gmail.com>
2025-10-14 21:17:31 -0600

    Map update: enemies now included when map is saved/loaded

--- io.js
+++ io.js
@@ -1,11 +1,12 @@
 // IO.JS: HANDLES LOADING + SAVING MAP DATA
 
 function save_map() {
-	text = JSON.stringify(walls);
+	save_walls = JSON.stringify(walls);
+	save_enemies = JSON.stringify(allEnemies);
 	name = "map";
 	type = "text/plain";
 
-	var file = new Blob([size + "\n" + text], {type: type});
+	var file = new Blob([size + "\n" + save_walls + "\n" + save_enemies], {type: type});
 	save_button.href = URL.createObjectURL(file);
 	save_button.download = name;
 }
@@ -22,11 +23,22 @@ function load_map() {
 
 		var lines = load.split('\n');
 		size = JSON.parse(lines[0]); // will need to be modified for sizeX and sizeY when they exist
-		var loaded = lines[1]; // reads the second line (map data)
+		var loaded_walls = lines[1]; // reads the second line (map data)
+		var loaded_enemies = lines[2]; // reads the third line (enemy/entity data)
 
 		//walls = [];
-		walls = JSON.parse(loaded);
-		update();
+		if (!loaded_walls) {
+			console.log("Failed to load map!");
+			return;
+		} else {
+			walls = JSON.parse(loaded_walls);
+			if (loaded_enemies) {
+				allEnemies = JSON.parse(loaded_enemies);
+			} else {
+				allEnemies = [];
+			}
+			update();
+		}
 	};
 	reader.readAsText(load);
 }
