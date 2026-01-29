f7f9b5fb5cac267abff776c5fc58f167c792299a
benzach0421 <benzach0421@gmail.com>
2026-01-29 16:41:52 -0700

    Minor wall rendering fixed. I aughta get these rendering calls streamlined...

--- turns.js
+++ turns.js
@@ -76,8 +76,8 @@ var turns = {
 			canvas.init();
 			canvas.clear();
 			canvas.grid();
-			canvas.walls();
 			canvas.items();
+			canvas.walls();
 			//canvas.drawGrenades();
 			canvas.player();
 			canvas.enemy();
