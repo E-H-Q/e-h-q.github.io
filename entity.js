66f10faa9653909c8f7f8bada92aee53ee202ad7
benzach0421 <benzach0421@gmail.com>
2026-05-01 21:08:59 -0600

    Traits update: Aggressive trait opens doors that block LOS, Defensive trait can close doors to block LOS, new life steal trait where dealing damage heals the attacker, (not for grenades).

--- entity.js
+++ entity.js
@@ -123,6 +123,8 @@ const EntitySystem = {
 					if (enemy.seenX !== undefined) { enemy.seenX = attacker.x; enemy.seenY = attacker.y; }
 					console.log(attacker.name + " hits " + enemy.name + " for " + dmg + " DMG!");
 					if (helper.hasTrait(attacker, 'lifesteal')) {
+						if (enemy.name == "Grenade" && enemy.active) return; // No lifesteal when shooting grenades
+						dmg = Math.min(dmg, enemy.hp);
 						attacker.hp += dmg;
 						console.log(attacker.name + " gained " + dmg + " HP from " + enemy.name);
 					}
